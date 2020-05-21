39ceca0a0f89469a165d18dadd615781a9f660f4
sunlidong <654460176@qq.com>
2020-05-21 14:17:12 +0800

    test

--- main.go
+++ main.go
@@ -1,23 +1,20 @@
 package main
 
+import "fmt"
+
 /**
 * Main
 * @lidong sun
 * @Time 2019年10月14日10:25:50
 *
  */
-import (
-	r_action "ki-sdk/router/action"
-	r_api "ki-sdk/router/api"
-)
 
 func main() {
 
-	server := r_api.InitRouter()
+	// server := r_api.InitRouter()
 
-	server.Run(r_action.Port_01)
-	log.
+	// server.Run(r_action.Port_01)
 
-		//	03. 提示启动成功
-		log.Println("服务启动成功.....")
+	fmt.Println("git test ")
+
 }
