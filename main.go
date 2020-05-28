f102d85a0e6d2f2c3112858aacadbdc7fff46c89
sunlidong <654460176@qq.com>
2020-05-28 16:21:19 +0800

    EndpointConfigImpls

--- main.go
+++ main.go
@@ -1,34 +1,32 @@
 package main
 
 import (
-
-	m "ki-sdk/model"
-	"ki-sdk/e2e"
 	"ki-sdk/configless"
+	"ki-sdk/e2e"
+
 	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
 )
 
 func main() {
 
 	// App
-	m.InitSDK()
- 
-		e2e.SetupAndRuning( nil,
-			fabsdk.WithEndpointConfig(configless.EndpointConfigImpls...),
-			fabsdk.WithCryptoSuiteConfig(configless.CryptoConfigImpls...),
-			fabsdk.WithIdentityConfig(configless.IdentityConfigImpls...),
-			fabsdk.WithMetricsConfig(configless.OperationsConfigImpls...),
-		)
-	}
+	// m.InitSDK()
 
-	// // 初始化路由
-	// egg := r.InitRouter()
-
-	// // 启动  server
-	// err := egg.Run(":8080")
-	// if err == nil {
-	// 	log.Println("egg is starting")
-	// } else {
-	// 	log.Println("egg is err:", err)
-	// }
+	e2e.SetupAndRuning(false, nil,
+		fabsdk.WithEndpointConfig(configless.EndpointConfigImpls...),
+		fabsdk.WithCryptoSuiteConfig(configless.CryptoConfigImpls...),
+		fabsdk.WithIdentityConfig(configless.IdentityConfigImpls...),
+		fabsdk.WithMetricsConfig(configless.OperationsConfigImpls...),
+	)
 }
+
+// // 初始化路由
+// egg := r.InitRouter()
+
+// // 启动  server
+// err := egg.Run(":8080")
+// if err == nil {
+// 	log.Println("egg is starting")
+// } else {
+// 	log.Println("egg is err:", err)
+// }
