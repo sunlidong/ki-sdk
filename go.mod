2954b733d66a1a2b7211e7135b0abf83eff4f6cc
sunlidong <654460176@qq.com>
2020-05-28 11:06:01 +0800

    sdk

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.14
 require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/hyperledger/fabric-sdk-go v1.0.0-beta1
-	github.com/pkg/errors v0.8.1
+	github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric v0.0.0-20190822125948-d2b42602e52e // indirect
+	github.com/pkg/errors v0.9.1
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	m "ki-sdk/model"
 	r "ki-sdk/router"
+	"ki-sdk/test/integration/e2e/configless"
 	"log"
 )
 
@@ -11,14 +12,17 @@ func main() {
 	// App
 	m.InitSDK()
 
+	//
+	configless.E2EBlockInit()
+
 	// 初始化路由
 	egg := r.InitRouter()
 
-	// 启动  server
-	err := egg.Run(":8080")
-	if err == nil {
-		log.Println("egg is starting")
-	} else {
-		log.Println("egg is err:", err)
-	}
-}
+// 	// 启动  server
+// 	err := egg.Run(":8080")
+// 	if err == nil {
+// 		log.Println("egg is starting")
+// 	} else {
+// 		log.Println("egg is err:", err)
+// 	}
+// }
