e095f1252b9c2e07df472459e8cfa780aac9a86b
sunlidong <654460176@qq.com>
2020-05-29 11:35:12 +0800

    1

--- install/end_to_end.go
+++ install/end_to_end.go
@@ -6,6 +6,21 @@ SPDX-License-Identifier: Apache-2.0
 
 package instll
 
+import (
+	"ki-sdk/e2e"
+	"ki-sdk/install"
+	"ki-sdk/vendor/github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
+)
+
 // this test mimics the original e2e test with the difference of injecting interface functions implementations
 // to programmatically supply configs instead of using a yaml file. With this change, application developers can fetch
 // configs from any source as long as they provide their own implementations.
+func InitApi() {
+	e2e.SetupAndRuning(false, nil,
+		fabsdk.WithEndpointConfig(install.EndpointConfigImpls...),
+		fabsdk.WithCryptoSuiteConfig(install.CryptoConfigImpls...),
+		fabsdk.WithIdentityConfig(install.IdentityConfigImpls...),
+		fabsdk.WithMetricsConfig(install.OperationsConfigImpls...),
+	)
+
+}
--- main.go
+++ main.go
@@ -1,23 +1,14 @@
 package main
 
 import (
-	"ki-sdk/configless"
-	"ki-sdk/e2e"
-
-	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
+	"ki-sdk/instll"
 )
 
 func main() {
 
 	// App
 	// m.InitSDK()
-
-	e2e.SetupAndRuning(false, nil,
-		fabsdk.WithEndpointConfig(configless.EndpointConfigImpls...),
-		fabsdk.WithCryptoSuiteConfig(configless.CryptoConfigImpls...),
-		fabsdk.WithIdentityConfig(configless.IdentityConfigImpls...),
-		fabsdk.WithMetricsConfig(configless.OperationsConfigImpls...),
-	)
+	instll.InitApi()
 }
 
 // // 初始化路由
