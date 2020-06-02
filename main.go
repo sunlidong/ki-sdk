d01879822040afa1f4c2e47ad0fd7353dd6463a5
sunlidong <654460176@qq.com>
2020-06-02 15:28:09 +0800

    2

--- main.go
+++ main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"ki-sdk/install"
+	"log"
 )
 
 func main() {
 
 	// App
-	// m.InitSDK()
+	m.InitSDK()
 	install.InitApi()
+
+	// App// 初始化路由
+	egg := r.InitRouter()
+
+	// 启动  server
+	err := egg.Run(":8080")
+	if err == nil {
+		log.Println("egg is starting")
+	} else {
+		log.Println("egg is err:", err)
+	}
+
 }
-
-// // 初始化路由
-// egg := r.InitRouter()
-
-// // 启动  server
-// err := egg.Run(":8080")
-// if err == nil {
-// 	log.Println("egg is starting")
-// } else {
-// 	log.Println("egg is err:", err)
-// }
