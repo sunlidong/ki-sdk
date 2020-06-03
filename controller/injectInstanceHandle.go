6aa2917c3f74694981533a23e0f6b9796dd1be7f
sunlidong <654460176@qq.com>
2020-06-03 16:02:36 +0800

    5

--- controller/injectInstanceHandle.go
+++ controller/injectInstanceHandle.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	m "ki-sdk/model"
+	"ki-sdk/vendor/github.com/hyperledger/fabric-sdk-go/pkg/client/resmgmt"
+	"log"
 )
 
 /*
@@ -36,7 +38,6 @@ func channelPropertyAccess(p *SystemByJoinChannelDb) error {
 	return err
 }
 
-
 // type SystemByXnNodeInfoListFreeDb struct {
 // 	PeerHost   string `json:"peerHost"`
 // 	ConfigFile string `json:"configFile"`
@@ -44,30 +45,29 @@ func channelPropertyAccess(p *SystemByJoinChannelDb) error {
 // }
 
 // 查询 某个 节点 已经 实例化 的 链码
-func xnEnumerateExistingNodes(p *SystemByXnNodeInfoListFreeDb) (list []string, err error ){
+func xnEnumerateExistingNodes(p *SystemByXnNodeInfoListFreeDb) (list []string, err error) {
 
-	resmgmt_client,err:=func()(*resmgmt.Client, error){
-		resmgmt_Client,err:= m.GetResmgmtClient(
+	resmgmt_client, err := func() (*resmgmt.Client, error) {
+		resmgmt_Client, err := m.GetResmgmtClient(
 			p.ConfigFile,
-			p.Org
-		 )
-		 if err !=nil{
-			log.Println("实例化 resmgmt_Client 失败： ",err)
+			p.Org,
+		)
+		if err != nil {
+			log.Println("实例化 resmgmt_Client 失败： ", err)
 
-		 }
+		}
 		return resmgmt_Client
 	}()
 	//
 	if err != nil {
-		log.Println("err:",err)
+		log.Println("err:", err)
 		return nil, err
 	}
 
-	arr,err1:=resmgmt_client.GetInstalledCC(p.PeerHost)
+	arr, err1 := resmgmt_client.GetInstalledCC(p.PeerHost)
 	if err != nil {
 		return nil, err
 	}
-	return  arr ,nil
-
+	return arr, nil
 
 }
